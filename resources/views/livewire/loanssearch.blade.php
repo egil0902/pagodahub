b3714f27d0102ef44abb3a2b33f236841ec41d5d
cris-casadelsoftware <cristopher@casadelsoftware.com>
2023-01-06 16:45:06 -0500

    Tabla de prestamos

--- resources/views/livewire/loanssearch.blade.php
+++ resources/views/livewire/loanssearch.blade.php
@@ -1,113 +1,114 @@
 <input type="hidden" class="form-control" placeholder="Search" wire:model="searchTerm" value="" />
-<div class="row row-cols-4 row-cols-sm-4 row-cols-md-4">
-    <div class="col-md border" align="center">
-        Tipo
-    </div>
-    <div class="col-md border" align="center">
-        Fecha
-    </div>
-    {{-- <div class="col-md border" align="justify">
-        Cedula
-    </div>
-    <div class="col-md border" align="justify">
-        Nombre
-    </div> --}}
-    <div class="col-md border" align="center">
-        Monto
-    </div>
-    <div class="col-md border" align="center">
-        Adjuntos
-    </div>
+<div class="table-responsive">
+    <table class="table table-bordered">
+        <thead id="miTablaPersonalizada">
 
-</div>
+            <th>Tipo</th>
+            <th>Fecha</th>
+            <th>Monto</th>
+            <th>Adjuntos</th>
 
-@if (isset($loans))
-    @if ($loans->isNotEmpty())
-        @foreach ($loans as $data)
-            <form name="valepagoda_destroy" id="valepagoda_destroy" method="POST"
-                action="{{ route('valepagoda.destroy') }}">
-                @csrf
-                <input name="valeid" type="hidden" value="{{ $data->id }}">
-                <div class="row row-cols-4 row-cols-sm-4 row-cols-md-4">
-                    <div class="col-md border" style="font-size: 14px" align="justify">
-                        {{ $data->loan_type }}
-                    </div>
-                    <div class="col-md border" style="font-size: 14px" align="justify">
-                        {{  date('d-m-Y', strtotime($data->datetrx)) }}
-                    </div>
-                    <div class="col-md border" style="font-size: 14px" align="right">$
-                        @php
-                            echo number_format($data->monto, 2, ',', ' ');
-                        @endphp
-                    </div>
-                    <div class="col-md border" style="font-size: 14px" align="justify">
-                        <!-- Button trigger modal -->
-                        <center>
-                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal{{ $data->id }}"> ver
-                                {{-- <img name="" width="25" height="25" src="img/ver.png"> --}}
-                            </button>
-                        </center>
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal{{ $data->id }}" tabindex="-1"
-                            aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-lg">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Abjuntos</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <div class="card h-100 w-100">
-                                            <div class="card card-body">
-                                                <img name="" src=" {{ $data->signatures }}" border="1">
+        </thead>
+        <tbody>
+            @if (isset($loans))
+                @if ($loans->isNotEmpty())
+                    @foreach ($loans as $data)
+                        {{-- <form name="valepagoda_destroy" id="valepagoda_destroy" method="POST"
+                            action="{{ route('valepagoda.destroy') }}">
+                            @csrf --}}
+                        <tr>
+                            <input name="valeid" type="hidden" value="{{ $data->id }}">
+                            <td> {{ $data->loan_type }}</td>
+                            <td>{{ date('d-m-Y', strtotime($data->datetrx)) }}</td>
+                            <td  align="right">
+                                <p>@php
+                                    echo number_format($data->monto, 2, ',', ' ');
+                                @endphp</p>
+                            </td>
+                            <td>
+                                <center>
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                        data-bs-target="#exampleModal{{ $data->id }}"> ver
+                                        {{-- <img name="" width="25" height="25" src="img/ver.png"> --}}
+                                    </button>
+                                </center>
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal{{ $data->id }}" tabindex="-1"
+                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Abjuntos</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="card h-100 w-100">
+                                                    <div class="card card-body">
+                                                        <img name="" src=" {{ $data->signatures }}"
+                                                            border="1">
+                                                    </div>
+                                                </div>
+                                                <div class="card h-100 w-100">
+                                                    <div class="card card-body">
+                                                        <img name=""
+                                                            src="data:image/png;base64,{{ $data->files }}"
+                                                            border="1">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <center>
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">Cerrar</button>
+                                                </center>
                                             </div>
                                         </div>
-                                        <div class="card h-100 w-100">
-                                            <div class="card card-body">
-                                                <img name="" src="data:image/png;base64,{{ $data->files }}"
-                                                    border="1">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <center>
-                                            <button type="button" class="btn btn-secondary"
-                                                data-bs-dismiss="modal">Cerrar</button>
-                                        </center>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <br>
-                </div>
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                    aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">Confirmacion</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                Seguro desea eliminar este registro?
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                <button type="submit" class="btn btn-primary">Confirmar</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        @endforeach
-    @else
-        @csrf
-    @endif
-@endif
+                            </td>
+                        </tr>
+                        {{-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                                aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Confirmacion</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Seguro desea eliminar este registro?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Cerrar</button>
+                                            <button type="submit" class="btn btn-primary">Confirmar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> --}}
+                        {{--  </form> --}}
+                    @endforeach
+                @else
+                    @csrf
+                @endif
+            @endif
+        </tbody>
+    </table>
+</div>
+<style>
+    #miTablaPersonalizada th {
+        width: 100px;
+        overflow: auto;
+        border: 1px solid;
+    }
+    table {
+                table-layout: fixed;
+            }
+</style> 
+
+
 {{-- <div class="row">
     <div class="col-md-12 col-12 col-sm-12 col-lg-12">
         {{ $loans->links() }}
