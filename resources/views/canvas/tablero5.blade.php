e256be357ec78e925dc4242a4c4e38cfc89149a4
cris-casadelsoftware <cristopher@casadelsoftware.com>
2023-01-13 13:06:36 -0500

    icono boton

--- resources/views/canvas/tablero5.blade.php
+++ resources/views/canvas/tablero5.blade.php
@@ -4,8 +4,16 @@
 <!-- creamos el form para el envio -->
 
 @csrf
-    
-    <input type='hidden' name='imagen5' id='imagen5' />
-    <br>
-<button class="btn btn-primary mh-100" type='button' onclick='LimpiarTrazado5()'>Borrar</button>
+
+<input type='hidden' name='imagen5' id='imagen5' />
+<br>
+<button type="button" class="btn btn-outline-danger" onclick='LimpiarTrazado5()'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eraser"
+        viewBox="0 0 16 16">
+        <path
+            d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm2.121.707a1 1 0 0 0-1.414 0L4.16 7.547l5.293 5.293 4.633-4.633a1 1 0 0 0 0-1.414l-3.879-3.879zM8.746 13.547 3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z">
+        </path>
+    </svg>
+    Borrar
+</button>
 <script src="js/paintfirma3.js"></script>
