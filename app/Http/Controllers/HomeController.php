9815a7669c25c42ef59dc279f63d480bbdbc934b
cris-casadelsoftware <cristopher@casadelsoftware.com>
2023-02-10 11:16:44 -0500

    permisos controlador home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,12 +30,20 @@ class HomeController extends Controller
         //dd($user);
 
         $APIController = new APIController();
-        $response = $APIController->getModel('AD_Org','','issummary eq true');
+        $response = $APIController->getModel('AD_Org', '', 'issummary eq true');
         $permisos = $APIController->getModel('PAGODAHUB_closecash', 'Name,AD_User_ID', '', '', '', '', '');
+
+        /* dd($permisos); */
+
+        $name_user = auth()->user()->name;
+        $email_user = auth()->user()->email;
+        $user = $APIController->getModel('AD_User', '', "Name eq '$name_user' and EMail eq '$email_user'", '', '', '', 'PAGODAHUB_closecash');
+        //dump($user);
+
         if (isset($response)) {
             //dd($response);
             $orgs =  $response;
-            return view('home', ['orgs' => $orgs,'permisos' => $permisos]);
+            return view('home', ['orgs' => $orgs, 'permisos' => $permisos, 'permisos2' => $user]);
         }
         return view('home');
     }
