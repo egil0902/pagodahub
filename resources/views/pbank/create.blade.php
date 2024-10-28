b1bac75f3438808cc586276376e8a7d69e30253d
JJGS2022 <jjgs.ve.2019@gmail.com>
2024-10-28 19:32:39 -0400

    loader presupuesto

--- resources/views/pbank/create.blade.php
+++ resources/views/pbank/create.blade.php
@@ -6,7 +6,14 @@
     <div class="alert alert-success">{{ session('mensaje') }}</div>
 @endif
 @section('content')
-<div class="p-2 m-0 border-0 bd-example">
+<div class="justify-content-center d-none container-loader" style="align-content: center; min-height: 100vh;">
+    <div class="d-flex justify-content-center">
+        <div class="spinner-border" style="width: 4rem; height: 4rem;" role="status">
+        </div>
+    </div>
+    <strong class="d-flex justify-content-center mt-3" style="font-size: 1.5rem;">Procesando la información, por favor espere...</strong>
+</div>
+<div class="p-2 m-0 border-0 bd-example container-form">
     <div class="d-flex">
         <!-- Formulario de búsqueda por proveedor -->
         <div class="container">
@@ -61,7 +68,7 @@
                         </div> 
                         <div class="form-group w-auto">
                             @csrf
-                            <button class=" w-100 btn btn-outline-secondary m-0" type="submit" id="button-addon2">Crear registro</button>
+                            <button class=" w-100 btn btn-outline-secondary m-0" type="button" id="button-addon2" onclick="enviarFormulario()">Crear registro</button>
                         </div>
                     </form>
                     <hr class="mb-4">                    
@@ -72,6 +79,21 @@
     </br>
     
 </div>
+
+<script>
+    
+    function enviarFormulario() {
+        // Aquí puedes realizar cualquier otra validación antes de enviar el formulario
+
+        $('#confirmModal').modal('hide');
+        $('.container-form').addClass('d-none');
+        $('.container-loader').removeClass('d-none');
+
+        // Envía el formulario
+        document.getElementById('provider').submit();
+    }
+
+</script>
 
     <style>
         .campo input {
